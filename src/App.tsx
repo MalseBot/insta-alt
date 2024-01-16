0e323e404aa46a6cf5fe322b8834d9ba006ecb2f
Eslam Hassanin <eso20016@gmail.com>
2024-01-16 16:33:25 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from 'react-router-dom'
+import './globals.css'
+import { Home } from './_root/Pages/'
+import SigninForm from './_auth/Froms/SigninForm'
+import SignupForm from './_auth/Froms/SignupForm'
+import AuthLayout from './_auth/AuthLayout'
+import RootLayout from './_root/RootLayout'
+
+const App = () => {
+  return (
+    <main>
+      <Routes>
+        {/*public*/}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignupForm />} />
+        </Route>
+
+        {/*private */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
