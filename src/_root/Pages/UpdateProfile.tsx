66ea9a69c3d1e179909f03082423eebbb9fdfd61
Eslam Hassanin <eso20016@gmail.com>
2025-05-09 22:07:28 +0300

    fix data

--- src/_root/Pages/UpdateProfile.tsx
+++ src/_root/Pages/UpdateProfile.tsx
@@ -2,10 +2,10 @@ import UpdateProfileForm from '@/components/Forms/UpdateProfileForm'
 import { useGetCurrentUser } from '@/lib/react-query/queriesAndMutations'
 
 function UpdateProfile() {
-  const { data: profile } = useGetCurrentUser()
-
+  const { data:profile } = useGetCurrentUser()
   return (
     <div className='w-full'>
+      {/* @ts-expect-error-bad  */}
       <UpdateProfileForm profile={profile} />
     </div>
   )
